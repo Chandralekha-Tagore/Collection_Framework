9b54addd9359dc22b5ccd7d3652b6089d6866f6a
Chandralekha-Tagore <60306715+Chandralekha-Tagore@users.noreply.github.com>
2020-07-25 23:09:04 +0530

    Add files via upload

---
+++ collectionframework/App.java
@@ -0,0 +1,13 @@
+package com.epam.Collections;
+
+/**
+ * Collection Framework
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Framework" );
+    }
+}
